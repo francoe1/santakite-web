93883debf9cec0fe433b54c860fe397a18b2e1dc
Franco Rosatto <francoe12012@gmail.com>
2025-12-04 14:43:33 -0300

    Add Vercel Analytics tracking (#8)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
 
 import App from './App.vue'
 import router from './router'
@@ -10,3 +11,7 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+if (typeof window !== 'undefined' && import.meta.env.PROD) {
+  inject({ framework: 'vue' })
+}
